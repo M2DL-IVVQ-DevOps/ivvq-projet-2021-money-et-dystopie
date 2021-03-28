0a4c7e382d7809a5ba5d1fe5442ebf49218558dd
Nicolas ANDRÉ <45142835+iomega11@users.noreply.github.com>
2021-03-28 12:51:44 +0200

    Ajout du backend de type hello-world

---
+++ moneyetdystopie-back/src/main/java/org/ups/m2dl/moneyetdystopieback/MoneyetdystopieBackApplication.java
@@ -0,0 +1,13 @@
+package org.ups.m2dl.moneyetdystopieback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyetdystopieBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyetdystopieBackApplication.class, args);
+    }
+
+}
