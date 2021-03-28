1058c9d7db5b46bca103580f32eb0dada8e9cac1
NinaNekonoran <franzese.rozen@gmail.com>
2021-03-28 16:06:24 +0200

    Les fichiers du frontend servis par le backend. Resolved  #4

---
+++ moneyetdystopie-back/src/main/java/org/ups/m2dl/moneyetdystopieback/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package org.ups.m2dl.moneyetdystopieback.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String helloworld() {
+        return "index";
+    }
+
+}
---
+++ moneyetdystopie-front/vue.config.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+module.exports = {
+    publicPath: './',
+    outputDir: path.resolve("static", "../../moneyetdystopie-back/src/main/resources/static"),
+
+}
