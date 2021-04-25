c26a80fb3025bbd5444c5f615b322ed23d20feee
Aymeric PINEL <aymeric.pinel@outlook.fr>
2021-04-25 18:06:15 +0200

    mise en place de Cypress avec Cucumber

---
+++ moneyetdystopie-front/cypress/integration/cart.feature
@@ -0,0 +1,14 @@
+Feature: Cart tests
+
+  I want to check cart behavior
+
+  Scenario: Adding a product to the cart
+    Given an empty cart
+    When I add an item to the cart
+    Then the cart size is 1
+
+  Scenario: Deleting a product from the cart
+    Given a cart with one item
+    When I delete the item from the cart
+    Then the cart size is 0
+
---
+++ moneyetdystopie-front/cypress/integration/cart/cart.js
@@ -0,0 +1,25 @@
+const {
+    Given,
+    When,
+    Then,
+} = require("cypress-cucumber-preprocessor/steps");
+
+Given(/^an empty cart$/, function () {
+
+});
+
+When(/^I add an item to the cart$/, function () {
+
+});
+
+Then(/^the cart size is (\d+)$/, function () {
+
+});
+
+Given(/^a cart with one item$/, function () {
+
+});
+
+When(/^I delete the item from the cart$/, function () {
+
+});
