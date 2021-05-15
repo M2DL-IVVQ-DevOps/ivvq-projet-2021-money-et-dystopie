160bb1365fc655c2f7930d47863ca9d2c47207df
Nicolas ANDRÉ <45142835+iomega11@users.noreply.github.com>
2021-05-15 14:35:24 +0200

    Ajout de la référence vers l'acheteur dans CommandBean

--- moneyetdystopie-back/src/main/java/org/ups/m2dl/moneyetdystopieback/bean/CommandBean.java
+++ moneyetdystopie-back/src/main/java/org/ups/m2dl/moneyetdystopieback/bean/CommandBean.java
@@ -1,11 +1,14 @@
 package org.ups.m2dl.moneyetdystopieback.bean;
 
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.ups.m2dl.moneyetdystopieback.enums.CommandState;
+
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,6 +23,11 @@ public class CommandBean {
     @Getter
     @Setter
     private CommandState state;
+
+    /** Acheteur. */
+    @Getter
+    @Setter
+    private CustomerBean customer;
 
     /** Liste des articles de la commande. */
     @Getter
