cb0cead90ea67d3aef625f51090c109575a88e06
Nicolas ANDRÉ <45142835+iomega11@users.noreply.github.com>
2021-05-15 16:29:43 +0200

    Ajout des méthodes getBean et getDto dans CustomerService

--- moneyetdystopie-back/src/main/java/org/ups/m2dl/moneyetdystopieback/services/CustomerService.java
+++ moneyetdystopie-back/src/main/java/org/ups/m2dl/moneyetdystopieback/services/CustomerService.java
@@ -1,22 +1,27 @@
 package org.ups.m2dl.moneyetdystopieback.services;
 
-import java.util.Iterator;
-import java.util.Set;
-import javax.validation.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.ups.m2dl.moneyetdystopieback.bean.CustomerBean;
 import org.ups.m2dl.moneyetdystopieback.domain.Customer;
 import org.ups.m2dl.moneyetdystopieback.exceptions.BusinessException;
 import org.ups.m2dl.moneyetdystopieback.repositories.CustomerRepository;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class CustomerService {
 
     @Getter
-    @Setter
     private final CustomerRepository customerRepository;
 
     public Customer create(Customer customer) throws BusinessException {
@@ -69,5 +74,31 @@ public class CustomerService {
             Iterator<ConstraintViolation<Customer>> iterator = constraintViolations.iterator();
             throw new BusinessException(iterator.next().getMessage());
         }
+    }
+
+    public static CustomerBean getBean(Customer customer) {
+        CustomerBean customerBean = new CustomerBean();
+        BeanUtils.copyProperties(customer, customerBean);
+        customerBean.setUserAccount(UserService.getBean(customer.getUserAccount()));
+        // FIXME La conversion Bean <-> DTO induit une boucle infinie.
+        //  Le code ci dessous est commenté pour éviter un StackOverFlow.
+        //  La conversion est incomplète du coup.
+        //if(customer.getPastCommands() != null) {
+        //    customerBean.setPastCommands(customer.getPastCommands().stream().map(OrderService::getBean).collect(Collectors.toList()));
+        //}
+        return customerBean;
+    }
+
+    public static Customer getDto(CustomerBean customerBean) {
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(customerBean, customer);
+        customer.setUserAccount(UserService.getDto(customerBean.getUserAccount()));
+        // FIXME La conversion Bean <-> DTO induit une boucle infinie.
+        //  Le code ci dessous est commenté pour éviter un StackOverFlow.
+        //  La conversion est incomplète du coup.
+        //if(customerBean.getPastCommands() != null) {
+        //    customer.setPastCommands(customerBean.getPastCommands().stream().map(OrderService::getDto).collect(Collectors.toList()));
+        //}
+        return customer;
     }
 }
