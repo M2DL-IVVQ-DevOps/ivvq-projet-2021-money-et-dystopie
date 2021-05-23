5fc900ceab825d08cc0af6b5be96696e29410617
Heptaines-Romain <romain@goyheneix.fr>
2021-05-23 17:05:14 +0200

    Ajout annontation DirtiesContext

--- moneyetdystopie-back/src/test/java/org/ups/m2dl/moneyetdystopieback/controllers/controllers_integration_test/TokenControllerCreateIntegrationTest.java
+++ moneyetdystopie-back/src/test/java/org/ups/m2dl/moneyetdystopieback/controllers/controllers_integration_test/TokenControllerCreateIntegrationTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.ups.m2dl.moneyetdystopieback.bean.UserBean;
 import org.ups.m2dl.moneyetdystopieback.domain.Customer;
@@ -26,6 +27,7 @@ import org.ups.m2dl.moneyetdystopieback.utils.MoneyDystopieConstants;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class TokenControllerCreateIntegrationTest {
 
     @Autowired
