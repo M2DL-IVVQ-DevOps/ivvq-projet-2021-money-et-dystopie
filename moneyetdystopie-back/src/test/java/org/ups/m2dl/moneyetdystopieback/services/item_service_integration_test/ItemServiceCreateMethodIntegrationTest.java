f6a130e384ddeb1d79231635be24424306e69304
Nicolas ANDRÉ <45142835+iomega11@users.noreply.github.com>
2021-05-25 20:07:39 +0200

    Fixed tests not passing in CICD

--- moneyetdystopie-back/src/test/java/org/ups/m2dl/moneyetdystopieback/services/item_service_integration_test/ItemServiceCreateMethodIntegrationTest.java
+++ moneyetdystopie-back/src/test/java/org/ups/m2dl/moneyetdystopieback/services/item_service_integration_test/ItemServiceCreateMethodIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.ups.m2dl.moneyetdystopieback.domain.Item;
 import org.ups.m2dl.moneyetdystopieback.domain.Seller;
 import org.ups.m2dl.moneyetdystopieback.domain.Token;
@@ -18,6 +19,7 @@ import org.ups.m2dl.moneyetdystopieback.services.UserService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ItemServiceCreateMethodIntegrationTest {
 
     @Autowired
